0dfe7835a7d8521cb0f0bdb1babd401221d9d6e8
Pranav M S <pranavms13@yahoo.com>
2024-02-28 20:27:36 +0530

    Updates

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-go build -o build/mDNSLocal-win-amd64/mDNSLocal.exe mDNSLocal
+go build -o build/mDNSLocal-win-amd64.exe mDNSLocal
 
 set GOOS=linux
 set GOARCH=amd64
-go build -o build/mDNSLocal-linux-amd64/mDNSLocal mDNSLocal
+go build -o build/mDNSLocal-linux-amd64 mDNSLocal
 
 set GOOS=darwin
 set GOARCH=amd64
-go build -o build/mDNSLocal-darwin-amd64/mDNSLocal mDNSLocal
+go build -o build/mDNSLocal-darwin-amd64 mDNSLocal
