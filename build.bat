fad3d38ab1138d6d747a1fa0361187412c16d4c6
Pranav M S <pranavms13@yahoo.com>
2024-02-28 18:52:30 +0530

    Updates

---
+++ build.bat
@@ -0,0 +1,9 @@
+go build -o build/mDNSLocal-win-amd64/mDNSLocal.exe mDNSLocal
+
+set GOOS=linux
+set GOARCH=amd64
+go build -o build/mDNSLocal-linux-amd64/mDNSLocal mDNSLocal
+
+set GOOS=darwin
+set GOARCH=amd64
+go build -o build/mDNSLocal-darwin-amd64/mDNSLocal mDNSLocal
